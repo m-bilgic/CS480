138884df8eb997b64c9909b0b5e76b79fb01421d
m-bilgic <mbilgic@iit.edu>
2014-10-22 09:09:14 -0500

    generate_data

--- project/generate_data.py
+++ project/generate_data.py
@@ -5,25 +5,18 @@ Created on Oct 20, 2014
 '''
 from __future__ import print_function
 
-from sklearn.datasets import make_classification
+
 from sklearn.linear_model import LogisticRegression
 from sklearn import svm
-from sklearn import cross_validation
-
-
-
-from sklearn import datasets
-from sklearn.cross_validation import train_test_split
-from sklearn.grid_search import GridSearchCV
-from sklearn.metrics import classification_report
-from sklearn.svm import SVC
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.metrics import accuracy_score
 
 import numpy as np
 
-def generate_binary_data(n_samples = 1000, l0_prob = 0.5, n_useful = 20, usefullness = (1000,1000), n_replicated = 0, n_random=0, seed=0):
+def generate_binary_data(n_samples = 1000, l0_prob = 0.5, n_useful = 20, usefullness = (1000,1000), n_product = 0, n_replicate = 0, n_random=0, seed=0):
     rg = np.random.RandomState(seed)
     
-    n_features = n_useful + n_replicated + n_random
+    n_features = n_useful + n_replicate + n_product + n_random
     
     u_f_p = [[],[]]  
     
@@ -34,6 +27,7 @@ def generate_binary_data(n_samples = 1000, l0_prob = 0.5, n_useful = 20, usefull
     X = np.zeros((n_samples, n_features))
     y = np.zeros(n_samples, dtype=np.int)
     
+    # useful features
     for i in range(n_samples):
         #sample a label
         label = int(rg.rand()<l0_prob)
@@ -41,7 +35,31 @@ def generate_binary_data(n_samples = 1000, l0_prob = 0.5, n_useful = 20, usefull
         
         for f in range(n_useful):
             f_prob = u_f_p[label][f]
-            X[i,f] = int(rg.rand() < f_prob)             
+            X[i,f] = int(rg.rand() < f_prob)
+    
+    f_index = n_useful
+    
+    # replicate the useful features
+    for f in range(f_index, f_index + n_replicate):
+        r = rg.randint(n_useful)
+        for i in range(n_samples):
+            X[i,f] = X[i,r]
+    
+    f_index += n_replicate
+    
+    # take the product of two features
+    for f in range(f_index, f_index + n_product):
+        f1 = rg.randint(f_index)
+        f2 = rg.randint(f_index)
+        for i in range(n_samples):
+            X[i,f] = X[i,f1]*X[i,f2]
+    
+    f_index += n_product
+    
+    # random features
+    for f in range(f_index, f_index + n_random):
+        for i in range(n_samples):
+            X[i,f] = rg.randint(2)   
       
     return X, y
 
@@ -49,40 +67,48 @@ if __name__ == '__main__':
     
     n = 2000
     
-    X, y = generate_binary_data(n_samples = n, n_useful = 100, usefullness=(50,50))  
+    X, y = generate_binary_data(n_samples = n, n_useful = 40, n_product = 40, n_replicate = 10, n_random = 10, usefullness=(20,20))  
     #X, y = make_classification(n_samples = n)
     
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=0)
+    ts = 1000
     
-    tuned_parameters = [{'kernel': ['rbf'], 'gamma': [1e-3, 1e-4, 0],
-                     'C': [1, 10, 100, 1000]},
-                    {'kernel': ['linear'], 'C': [1, 10, 100, 1000]}]
+    X_train = X[:ts]
+    y_train = y[:ts]
     
-    scores = ['accuracy']
-
-    for score in scores:
-        print("# Tuning hyper-parameters for %s" % score)
-        print()
+    X_test = X[ts:]
+    y_test = y[ts:]
     
-        clf = GridSearchCV(SVC(C=1), tuned_parameters, cv=5, scoring=score)
+    classifiers = []
+    
+    classifiers.append(BernoulliNB())
+    classifiers.append(LogisticRegression(C=0.1))
+    classifiers.append(LogisticRegression(C=1))
+    classifiers.append(LogisticRegression(C=10))
+    classifiers.append(svm.SVC(kernel='rbf', C=0.1, gamma=0.0))
+    classifiers.append(svm.SVC(kernel='rbf', C=1, gamma=0.0))
+    classifiers.append(svm.SVC(kernel='rbf', C=10, gamma=0.0))
+    classifiers.append(svm.SVC(kernel='rbf', C=0.1, gamma=0.1))
+    classifiers.append(svm.SVC(kernel='rbf', C=1, gamma=0.1))
+    classifiers.append(svm.SVC(kernel='rbf', C=10, gamma=0.1))
+    
+    max_accu = 0
+    the_classifier = None
+    
+    for clf in classifiers:
         clf.fit(X_train, y_train)
-    
-        print("Best parameters set found on development set:")
-        print()
-        print(clf.best_estimator_)
-        print()
-        print("Grid scores on development set:")
-        print()
-        for params, mean_score, scores in clf.grid_scores_:
-            print("%0.3f (+/-%0.03f) for %r"
-                  % (mean_score, scores.std() / 2, params))
-        print()
-    
-        print("Detailed classification report:")
-        print()
-        print("The model is trained on the full development set.")
-        print("The scores are computed on the full evaluation set.")
-        print()
         y_true, y_pred = y_test, clf.predict(X_test)
-        print(classification_report(y_true, y_pred))
+       
+        accu = accuracy_score(y_true, y_pred)
+        if max_accu < accu:
+            max_accu = accu
+            the_classifier = clf 
+        print(clf)
+        print(accu)
         print()
+    
+    
+    print()
+    print()
+    print("The classifier")
+    print(the_classifier)
+    print(max_accu)
