3f4be62973f1e14d261ed84ae9f235f08638a964
Mustafa Bilgic <mbilgic@iit.edu>
2014-11-17 09:59:09 -0600

    load_data_generate_prices

---
+++ project/phase4/load_data_generate_prices.py
@@ -0,0 +1,52 @@
+'''
+Created on Nov 11, 2014
+
+@author: mbilgic
+'''
+
+import numpy as np
+from sklearn.datasets import dump_svmlight_file
+
+if __name__ == '__main__':
+    
+    data_path = './../phase2/'
+    data_group = "dataset8"
+    seed = 0
+    save = True
+    
+    X_tr = np.loadtxt(data_path + data_group +  "_X_train.csv", dtype=float, delimiter=',')
+    X_val = np.loadtxt(data_path + data_group +  "_X_val.csv", dtype=float, delimiter=',')
+    y_tr = np.loadtxt(data_path + data_group +  "_y_train.csv", dtype=float, delimiter=',')
+    y_val = np.loadtxt(data_path + data_group +  "_y_val.csv", dtype=float, delimiter=',')
+    
+    X = np.vstack((X_tr, X_val))
+    y = np.hstack((y_tr, y_val))
+    
+    rs = np.random.RandomState(seed)
+    
+    rnd_indices = rs.permutation(X.shape[0])
+    
+    X = X[rnd_indices]
+    y = y[rnd_indices]
+    
+    value = 100
+    
+    n = X.shape[0]
+    
+    prices = np.zeros(n)
+    
+    for i in range(n):
+        prices[i] = rs.rand()*value
+ 
+    if save:
+        print "Saving dataset: %s" %data_group      
+        
+        np.savetxt(data_group+"_X.csv", X, delimiter=',', fmt='%0.5f')
+        
+        np.savetxt(data_group+"_y.csv", y, delimiter=',', fmt='%d')
+        
+        np.savetxt(data_group+"_p.csv", prices, delimiter=',', fmt='%0.2f')
+        
+        dump_svmlight_file(X, y, data_group+"_X_y.txt")
+    
+    
